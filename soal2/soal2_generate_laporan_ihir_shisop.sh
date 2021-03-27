c89f7f8687b511890a5572b02d2d92f6f64f573e
iwandepe <iwan.dprakoso@gmail.com>
2021-03-27 08:14:50 +0700

    update solution 2

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -12,11 +12,11 @@ NR > 1{
 	}
 } 
 END {
-	print "Transaksi terakhir dengan profit percentage terbesar yaitu", order_id, "dengan persentase", max_percentage
+	print "Transaksi terakhir dengan profit percentage terbesar yaitu", order_id, "dengan persentase", max_percentage"%"
 }
-' Laporan-TokoShiSop.tsv
+' Laporan-TokoShiSop.tsv >> hasil.txt
 
-echo ''
+echo '' >> hasil.txt
 
 awk -F $'\t' '
 BEGIN {
@@ -25,17 +25,17 @@ BEGIN {
 /2017/ {
 	if ($10=="Albuquerque") print $7
 }
-' Laporan-TokoShiSop.tsv | uniq
+' Laporan-TokoShiSop.tsv | uniq >> hasil.txt
 
-echo ''
+echo '' >> hasil.txt
 
 awk -F $'\t' '
 BEGIN {
 	min=1000000; 
 	min_segment='null'
 } 
-NR>1{
-	arr[$8]++;
+NR > 1 {
+	arr[$8]+=$19;
 } 
 END {
 	for(a in arr) {
@@ -44,27 +44,27 @@ END {
 			min_segment=a
 		}
 	} 
-	print "Tipe segment customer yang penjualannya paling sedikit adalah ", min_segment, "dengan",  min, "transaksi"
+	print "Tipe segment customer yang penjualannya paling sedikit adalah", min_segment, "dengan",  min, "transaksi"
 }
-' Laporan-TokoShiSop.tsv
+' Laporan-TokoShiSop.tsv >> hasil.txt
 
-echo ''
+echo '' >> hasil.txt
 
 awk -F $'\t' '
 BEGIN {
 	min_profit=99999999; 
-	min_region='null'
 } 
-NR>1 {
-	arr[$11]+=$21
+NR > 1{
+	arr[$13]+=$21;
 } 
 END {
 	for(a in arr) {
-		if (arr[a]<min) { 
+		if (arr[a]<min_profit) { 
+
 			min_profit=arr[a]; 
 			min_region=a;
 		}
 	} 
 	print "Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah",  min_region, "dengan total keuntungan ", min_profit
 }
-' Laporan-TokoShiSop.tsv
+' Laporan-TokoShiSop.tsv >> hasil.txt
